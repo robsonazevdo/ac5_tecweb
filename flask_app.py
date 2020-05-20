6e13ca82d3f9a7478f05a491f545f2339f4e2d59
robsonazevdo <kassapete@hotmail.com>
2020-05-19 22:31:18 -0300

    ac05 final

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -12,13 +12,13 @@ db = SQLAlchemy(app)
 
 
 class Contato(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(10), unique=True, nullable=False)
-    sobrenome = db.Column(db.String(25), unique=True, nullable=False)
-    email = db.Column(db.String(35), unique=True, nullable=False)
-    assunto = db.Column(db.String(15), unique=True, nullable=False)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    nome = db.Column(db.String(10), nullable=True)
+    sobrenome = db.Column(db.String(25), nullable=True)
+    email = db.Column(db.String(35), nullable=True)
+    assunto = db.Column(db.String(15), nullable=True)
     hora = db.Column(db.DateTime, default=datetime.now)
-    mensagem = db.Column(db.String(150), unique=True, nullable=False)
+    mensagem = db.Column(db.String(150),nullable=True)
 
     db.create_all()
 
@@ -27,10 +27,19 @@ class Contato(db.Model):
 def contato():
     return  render_template('login.html')
 
-@app.route('/api/seus_dados', methods=['GET','POST'])
-#def seus_dados():
 
+@app.route('/seus_dados', methods=['GET','POST'])
+def seus_dados():
+    if request.method == 'POST':
+        print(request.form['nome'])
 
+        usuario = Contato(nome=request.form['nome'], sobrenome=request.form['sobrenome'], 
+                          email=request.form['email'], assunto=request.form['assunto'], 
+                          mensagem=request.form['mensagem'])
+        db.session.add(usuario)
+        db.session.commit()
+
+        return  render_template('obrigado.html')
 
 
 @app.route("/obrigado")
